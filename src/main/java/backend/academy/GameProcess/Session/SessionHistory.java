b16a63582a146c7e9164e50d6e3552298f6e33ef
GG_Host <gd.host@yandex.ru>
2024-09-11 17:44:43 +0300

    created history of the session covered by tests

---
+++ src/main/java/backend/academy/GameProcess/Session/SessionHistory.java
@@ -0,0 +1,51 @@
+package backend.academy.GameProcess.Session;
+
+import backend.academy.Words.Level;
+import lombok.Getter;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+@Getter
+public class SessionHistory {
+    private final List<String> passedCategories;
+    private final HashMap<Level, List<String>> passedLevelsOfCategories;
+    private final HashSet<String> passedWords;
+
+    public SessionHistory() {
+        passedCategories = new ArrayList<>();
+        passedLevelsOfCategories = new HashMap<>();
+        init();
+        passedWords = new HashSet<>();
+    }
+
+    private void init() {
+        passedLevelsOfCategories.put(Level.EASY, new ArrayList<>());
+        passedLevelsOfCategories.put(Level.MEDIUM, new ArrayList<>());
+        passedLevelsOfCategories.put(Level.HARD, new ArrayList<>());
+    }
+
+    public List<String> addPassedCategory(String category) {
+        passedCategories.add(category);
+        return passedCategories;
+    }
+
+    public HashMap<Level, List<String>> addPassedLevelForCategory(String category, Level level) {
+        passedLevelsOfCategories.get(level).add(category);
+        return passedLevelsOfCategories;
+    }
+
+    public HashSet<String> addPassedWord(String word) {
+        passedWords.add(word);
+        return passedWords;
+    }
+
+    public Set<Level> getPassedLevelsForCategory(String category) {
+        return new HashSet<>(passedLevelsOfCategories.keySet().stream().filter(l -> passedLevelsOfCategories.get(l).contains(category)).collect(
+            Collectors.toCollection(HashSet::new)));
+    }
+
+}
